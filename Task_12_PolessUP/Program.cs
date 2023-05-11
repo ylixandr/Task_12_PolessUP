3d00c6ca43771807b2ae56f9e3739b55151fd221
Ylixandr <savoshinsky@inbox.ru>
2023-05-11 19:24:24 +0300

    Добавьте файлы проекта.

---
+++ Task_12_PolessUP/Program.cs
@@ -0,0 +1,23 @@
+﻿string str = "3-598-21507-X";
+int sum = 0;
+int a = 10;
+
+foreach (char c in str)
+{
+    if (char.IsDigit(c))
+    {
+        sum += int.Parse(c.ToString()) * a;
+        a--;
+    }
+    else if (c == 'X' && a == 1) // если последний символ - X и остался только один шаг
+    {
+        sum += 10;
+    }
+}
+
+if (sum % 11 == 0)
+{
+    Console.WriteLine("Правильно");
+
+}
+else Console.WriteLine("Nope");
